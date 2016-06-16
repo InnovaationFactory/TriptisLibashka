518189daa8322ab0272cbca74b81627deb5b02dc
GGN <ggnair.engg@gmail.com>
2016-06-16 09:33:10 +0530

    Completed login workflow from UI. Server side token validation and user registration pending

--- UIManager/public/src/util/globalController.js
+++ UIManager/public/src/util/globalController.js
@@ -4,11 +4,23 @@
 
     angular
         .module('util')
-        .controller('globalController', ['$rootScope', '$routeParams', '$mdSidenav', '$mdBottomSheet', '$log', '$q', '$scope', '$window', globalController])
+        .controller('globalController', ['$rootScope', '$routeParams', '$mdSidenav', '$mdBottomSheet', '$log', '$q', '$scope', '$window', 'facebookService', globalController])
         .factory('facebookService', ['$q', facebookLoginService]);
 
-    function globalController($rootScope, $routeParams, $mdSidenav, $mdBottomSheet, $log, $q, $scope, $window) {
+    function globalController($rootScope, $routeParams, $mdSidenav, $mdBottomSheet, $log, $q, $scope, $window, facebookService) {
         var self = this;
+        self.user = {
+            Name: "Guest"
+        };
+
+        var setUser = function (userDetail) {
+            debugger;
+            self.user = {
+                Name: userDetail.name,
+                isAuthenticated: true
+            };
+        };
+
         $window.fbAsyncInit = function () {
             FB.init({
                 appId: '681826728631616',
@@ -18,23 +30,52 @@
                 version: 'v2.4'
             });
         };
-    }
 
+        var checkLoginStatus = function () {
+            facebookService.getLoginStatus(function (response) {
+                if (response.status != "connected") {
+                    return alert("Unable to login to facebook, Please check your credentials");
+                }
+                facebookService.getMyDetails(function (err, user) {
+                    if (err) {
+                        return alert("Unable to login to facebook, Please try again later");
+                    }
+                    setUser(user);
+                });
+            });
+        }
+
+        //facebookService.getMyDetails(function (err, user) {
+        //    if (!err) {
+        //        setUser(user);
+        //    }
+        //});
+
+        $rootScope.$on('FBUserLoggedIn', function () {
+            checkLoginStatus();
+        });
+
+        checkLoginStatus();
+    }
 
     function facebookLoginService($q) {
         return {
-            getMyLastName: function () {
-                var deferred = $q.defer();
+            getMyDetails: function (callback) {
                 FB.api('/me', {
-                    fields: 'last_name'
-                }, function (response) {
-                    if (!response || response.error) {
-                        deferred.reject('Error occured');
-                    } else {
-                        deferred.resolve(response);
-                    }
+                        fields: 'name,email,birthday,hometown,location'
+                    },
+                    function (response) {
+                        if (!response || response.error) {
+                            callback('Error occured', null);
+                        } else {
+                            callback(null, response);
+                        }
+                    });
+            },
+            getLoginStatus: function (callback) {
+                FB.getLoginStatus(function (response) {
+                    callback(response);
                 });
-                return deferred.promise;
             }
         }
     }
